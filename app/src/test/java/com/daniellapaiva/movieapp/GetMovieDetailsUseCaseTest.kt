c0e567fe8ed59dfdcaa4030b0a8f067d2136aeab
daniellaPaiva <daniellaspaiva@gmail.com>
2024-09-13 09:55:08 -0300

    test(usecase): add unit tests for GetMovieDetailsUseCase
    - Created GetMovieDetailsUseCaseTest to verify the functionality of fetching movie details.
    - Mocked MovieRepository to simulate API behavior and validate use case logic.

---
+++ app/src/test/java/com/daniellapaiva/movieapp/GetMovieDetailsUseCaseTest.kt
@@ -0,0 +1,39 @@
+package com.daniellapaiva.movieapp
+
+import com.daniellapaiva.movieapp.domain.MovieRepository
+import com.daniellapaiva.movieapp.domain.model.Movie
+import com.daniellapaiva.movieapp.domain.usecase.GetMovieDetailsUseCase
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class GetMovieDetailsUseCaseTest {
+
+    private lateinit var getMovieDetailsUseCase: GetMovieDetailsUseCase
+    private val movieRepository: MovieRepository = mockk()
+
+    @Before
+    fun setUp() {
+        getMovieDetailsUseCase = GetMovieDetailsUseCase(movieRepository)
+    }
+
+    @Test
+    fun `should return movie details when movieId is provided`() = runBlocking {
+        val movieId = 123
+        val expectedMovie = Movie(
+            id = 123,
+            title = "Example Movie",
+            overview = "An example movie overview",
+            posterPath = "/path_to_poster.jpg"
+        )
+
+        coEvery { movieRepository.getMovieDetails(movieId) } returns expectedMovie
+
+        val result = getMovieDetailsUseCase(movieId)
+
+        assertEquals(expectedMovie, result)
+    }
+}
