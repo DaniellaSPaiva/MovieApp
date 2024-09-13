aafb3084640efa784be58b38eb89e89a28397943
daniellaPaiva <daniellaspaiva@gmail.com>
2024-09-13 15:14:54 -0300

    fix(domain): add readResolve method to AppError sealed class for proper serialization
    - Implemented readResolve method in NetworkError and TimeoutError objects to ensure proper deserialization and maintain singleton behavior.
    - Resolved serialization warnings related to sealed class AppError.
    - Ensured consistent handling of AppError objects during the serialization/deserialization process.

--- app/src/main/java/com/daniellapaiva/movieapp/domain/util/AppError.kt
+++ app/src/main/java/com/daniellapaiva/movieapp/domain/util/AppError.kt
@@ -1,7 +1,15 @@
 package com.daniellapaiva.movieapp.domain.util
 
 sealed class AppError : Throwable() {
-    data object NetworkError : AppError()
-    data object TimeoutError : AppError()
+
+    data object NetworkError : AppError() {
+        private fun readResolve(): Any = NetworkError
+    }
+
+    data object TimeoutError : AppError() {
+        private fun readResolve(): Any = TimeoutError
+    }
+
     data class UnknownError(override val message: String?) : AppError()
+
 }
