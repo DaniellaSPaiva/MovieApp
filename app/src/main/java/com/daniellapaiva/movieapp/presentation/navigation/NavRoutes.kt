540c698a05ab5f2438c17afb156f761d6e10f02d
daniellaPaiva <daniellaspaiva@gmail.com>
2024-09-13 14:53:55 -0300

    refactor(ui): update NavGraph for improved readability and navigation handling
    - Separated navigation routes into dedicated functions for better modularity and readability.
    - Enhanced navigation to MovieDetailScreen by handling null movieId with proper error messaging.
    - Ensured consistent and maintainable navigation structure across the app.

--- app/src/main/java/com/daniellapaiva/movieapp/presentation/navigation/NavRoutes.kt
+++ app/src/main/java/com/daniellapaiva/movieapp/presentation/navigation/NavRoutes.kt
@@ -1,9 +1,10 @@
 package com.daniellapaiva.movieapp.presentation.navigation
 
 object NavRoutes {
+    const val MOVIE_ID_ARG = "movieId"
     const val MOVIE_LIST = "movie_list"
-    private const val MOVIE_DETAILS = "movie_details"
 
+    private const val MOVIE_DETAILS = "movie_details"
     const val MOVIE_DETAILS_WITH_ID = "$MOVIE_DETAILS/{movieId}"
     fun movieDetailsRoute(movieId: Int): String = "$MOVIE_DETAILS/$movieId"
 }
